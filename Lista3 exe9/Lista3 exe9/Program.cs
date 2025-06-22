8206263d74232bfcdf76e9df5eacff8f93a6e9d6
Ana Carolina Santos <149174247+Carolrbs@users.noreply.github.com>
2025-06-22 16:25:38 -0300

    Add files via upload

---
+++ Lista3 exe9/Lista3 exe9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_exe9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Os trinta primeiros valores da série de Fibonacci:");
+
+            
+            int a = 1;
+            int b = 1;
+
+            Console.Write(a + ", " + b);
+
+            for (int i = 2; i < 30; i++) 
+            {
+                int temp = a + b; 
+                Console.Write(", " + temp);
+
+                a = b;
+                b = temp; 
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+    }
+}
